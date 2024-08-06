661020190bcab1371c29f3a14328faedaa5c26e6
Rasio Atmaja <rasioatmaja29@gmail.com>
2024-08-07 06:40:14 +0700

    feat: add script login and connect with value from env (#7)

--- main.go
+++ main.go
@@ -16,12 +16,43 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// start nordvpn daemon
+	log.Println("Starting nordvpn daemon...")
+	_, err := exec.Command("/etc/init.d/nordvpn", "start").Output()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println("Checking nordvpn status...")
+	out, err := exec.Command("nordvpn", "status").Output()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("%s \n", out)
+
+	log.Println("Starting nordvpn login with token...")
+	out, err = exec.Command("nordvpn", "login", "--token", os.Getenv("NORDVPN_TOKEN")).Output()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("%s \n", out)
+
+	log.Printf("Connecting nordvpn to %s \n", os.Getenv("NORDVPN_DEFAULT_CONNECT_COUNTRY"))
+	out, err = exec.Command("nordvpn", "c", os.Getenv("NORDVPN_DEFAULT_CONNECT_COUNTRY")).Output()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("%s \n", out)
+
 	// define signal interrupt
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	// define error group
-	proc, gctx := errgroup.WithContext(ctx)
+	proc, _ := errgroup.WithContext(ctx)
 
 	// wait for signal
 	proc.Go(func() error {
@@ -31,17 +62,6 @@ func main() {
 
 		// cancel context after reveived signal interrupt
 		cancel()
-
-		return nil
-	})
-
-	proc.Go(func() error {
-		// start nordvpn daemon
-		log.Println("Starting nordvpn daemon...")
-		_, err := exec.CommandContext(gctx, "/etc/init.d/nordvpn", "start").Output()
-		if err != nil {
-			return err
-		}
 
 		return nil
 	})
