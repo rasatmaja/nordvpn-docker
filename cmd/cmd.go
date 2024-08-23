73abe9d7ffcbc5a5a55931d14d58773f6d2c3719
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2024-08-23 09:36:40 +0700

    fix: error stdout already set when reuse command

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,12 +7,25 @@ import (
 
 var (
 	// NordVPN Daemon
-	nordVPNDaemonStatus = exec.Command("/etc/init.d/nordvpn", "status")
-	nordVPNDaemonStart  = exec.Command("/etc/init.d/nordvpn", "start")
+	nordVPNDaemonStatus = command("/etc/init.d/nordvpn", "status")
+	nordVPNDaemonStart  = command("/etc/init.d/nordvpn", "start")
 
 	// NordVPN App
-	nordVPNAppStatus  = exec.Command("nordvpn", "status")
-	nordVPNAppAccount = exec.Command("nordvpn", "account")
-	nordVPNAppLogin   = exec.Command("nordvpn", "login", "--token", os.Getenv("NORDVPN_TOKEN"))
-	nordVPNAppConnect = exec.Command("nordvpn", "c", os.Getenv("NORDVPN_DEFAULT_CONNECT_COUNTRY"))
+	nordVPNAppStatus  = command("nordvpn", "status")
+	nordVPNAppAccount = command("nordvpn", "account")
+	nordVPNAppLogin   = command("nordvpn", "login", "--token", os.Getenv("NORDVPN_TOKEN"))
+	nordVPNAppConnect = command("nordvpn", "c", os.Getenv("NORDVPN_DEFAULT_CONNECT_COUNTRY"))
 )
+
+type cmd struct {
+	name string
+	args []string
+}
+
+func command(name string, args ...string) cmd {
+	return cmd{name: name, args: args}
+}
+
+func (c cmd) Output() ([]byte, error) {
+	return exec.Command(c.name, c.args...).Output()
+}
