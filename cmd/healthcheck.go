ec953a3a134a7f07b200ad4861d324ffefbfba0e
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2025-01-26 12:21:05 +0700

    refactor: process raw ouput using string split to generate map key value status instead of using regrex

--- cmd/healthcheck.go
+++ cmd/healthcheck.go
@@ -1,10 +1,9 @@
 package cmd
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
-	"regexp"
+	"strings"
 )
 
 // Healtcheck --
@@ -14,27 +13,45 @@ func Healtcheck() {
 		slog.Error(string(out))
 		os.Exit(1)
 	}
-	status := findStatus("Status", string(out))
-	server := findStatus("Server", string(out))
-	ip := findStatus("IP", string(out))
+
+	data := parseStatus(string(out))
+	status := data["status"]
 
 	if status != "Connected" {
 		slog.Error("NordVPN Not Connected", slog.String("status", status))
 		os.Exit(1)
 	}
 
+	server := data["server"]
+	ip := data["ip"]
 	slog.Info("NordVPN Connected", slog.String("server", server), slog.String("ip", ip))
 }
 
-const pattern = `(?i)%s:\s*(.*)`
+// Parse output of NordVPN status
+// ========Example raw output:==========
+// Status: Connected
+// Server: Singapore #123
+// Hostname: xxxx.nordvpn.com
+// IP: 192.166.246.xxx
+// Country: Singapore
+// City: Singapore
+// Current technology: NORDLYNX
+// Current protocol: UDP
+// Post-quantum VPN: Disabled
+// Transfer: 92 B received, 180 B sent
+// Uptime: 1 second
+// =====================================
+func parseStatus(input string) map[string]string {
+	lines := strings.Split(input, "\n")
+	status := make(map[string]string, len(lines))
 
-func findStatus(field, input string) string {
-	pattern := fmt.Sprintf(pattern, field)
-	key := regexp.MustCompile(pattern)
-	output := key.FindStringSubmatch(input)
-
-	if len(output) > 0 {
-		return output[1]
+	for _, line := range lines {
+		data := strings.Split(line, ":")
+		if len(data) > 1 {
+			key := strings.ToLower(data[0])
+			value := strings.TrimSpace(data[1])
+			status[key] = value
+		}
 	}
-	return ""
+	return status
 }
